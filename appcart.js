c91625ef8671c0de195d7708bc27b8aa0039c68e
Nick Gibson <Nicholas.a.Gibson@Gmail.com>
2017-10-05 16:05:04 -0700

    render from constructor working

--- appcart.js
+++ appcart.js
@@ -2,10 +2,9 @@
 
 //get data from localStore
 //TEST object! Remove later!
-function Product(src, count, name) {
+function Product(src, count) {
   this.source = src;
   this.count = count;
-  this.name = name;
   Product.orderInfo.push(this);
 };
 Product.orderInfo = [];
@@ -16,11 +15,12 @@ new Product('img/shark.jpg', 5, 'shark');
 console.table(Product.orderInfo);
 //display data from localStore to the DOM
 function makeCart(){
+  var buildList = document.getElementById('cartList').innerHTML;
   for (var i = 0; i < Product.orderInfo.length; i++) {
-    console.log('in for loop');
-    var buildList = '<li id="test"> <img src="' + Product.orderInfo[i].source + '" /> Item Count: ' + Product.orderInfo[i].count + ' <button type="button" name="deleteItem' + Product.orderInfo[i].name + '">Delete yourself</button> </li>';
-    document.getElementById('cartList').innerHTML = buildList;
+    buildList = buildList.concat('<li id="test"> <img src="' + Product.orderInfo[i].source + '" /> Item Count: ' + Product.orderInfo[i].count + ' <button type="button" name="deleteItem' + i + '">Delete yourself</button> </li>');
+    console.log(buildList);
   }
+  document.getElementById('cartList').innerHTML = buildList;
 }
 //erase an item from the cart AND DOM AND localStore via button click
 
