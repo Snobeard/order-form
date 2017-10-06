0973c57032a6b09d58e67509148b89c4066364c4
Nick Gibson <Nicholas.a.Gibson@Gmail.com>
2017-10-05 17:17:00 -0700

    not clean but FUNCTIONAL

--- appcart.js
+++ appcart.js
@@ -1,5 +1,11 @@
 'use strict';
+var cart = [];
+var quantity = [];
 
+if (localStorage.cart) {
+  cart = JSON.parse(localStorage.cart);
+  quantity = JSON.parse(localStorage.quantity);
+}
 //get data from localStore
 //TEST object! Remove later!
 function Product(src, count) {
@@ -9,20 +15,25 @@ function Product(src, count) {
 };
 Product.orderInfo = [];
 
-new Product('img/boots.jpg', 2, 'boots');
-new Product('img/shark.jpg', 5, 'shark');
+for (var i = 0; i < cart.length; i++) {
+  new Product(cart[i], quantity[i]);
+}
 
-console.table(Product.orderInfo);
 //display data from localStore to the DOM
 function makeCart(){
   var buildList = document.getElementById('cartList').innerHTML;
   for (var i = 0; i < Product.orderInfo.length; i++) {
-    buildList = buildList.concat('<li id="test"> <img src="' + Product.orderInfo[i].source + '" /> Item Count: ' + Product.orderInfo[i].count + ' <button type="button" name="deleteItem' + i + '">Delete yourself</button> </li>');
+    buildList = buildList.concat('<li id="test"> <img src="' + Product.orderInfo[i].source + '" /> Item Count: ' + Product.orderInfo[i].count + ' <button type="button" name="' + i + '" onClick="deleteString(this.name)">Delete yourself</button> </li>');
     console.log(buildList);
   }
   document.getElementById('cartList').innerHTML = buildList;
 }
-//erase an item from the cart AND DOM AND localStore via button click
+//erase an item localStore via button click
 
 //call stuff, you dolt!
 makeCart();
+function deleteString(name){
+  cart.splice(name, 1);
+  localStorage.cart = JSON.stringify(cart);
+  location.reload();
+}
